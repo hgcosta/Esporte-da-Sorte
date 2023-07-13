ee0b2191747ce4d2737c3cad3c80efbd2510d088
hgcosta <hugocosta.devs@gmail.com>
2023-07-13 13:15:58 -0300

    feat: estrutura html e css

--- page-home.php
+++ page-home.php
@@ -1,14 +1,164 @@
 <?php // Template Name: Home ?> 
 <?php get_header()?>
-<section class="" id="">
-   
-</section>
 
-<!-- <script>
+<main>
+    <section class="hero" id="">
+        <header>
+            <div class="container content-header">
+                <div class="logo">
+                    <a href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/logo-esporte-da-sorte-header.png" alt="Logo Esporte da Sorte"></a>
+                </div>
+                <nav class="desktop-nav">
+                    <ul class="list-unstyled">
+                        <li><a href="">Times</a></li>
+                        <li><a href="">Competições</a></li>
+                        <li><a href="">Eventos</a></li>
+                        <li><a href="">Redes Sociais</a></li>
+                        <li><a href="">Televisão</a></li>
+                        <li><a href="">Benefícios</a></li>
+                        <li><a href="">Seja um</br>Afiliado</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+       <div class="bg-fundo">
+            <div class="container content-hero">
+                <div class="col-info">
+                    <h1>Em <strong>campo,</strong> </br>a gente <strong>Entra</strong> pra vencer</h1>
+                    <p>Somos <strong>patrocinadores oficiais</strong> de alguns dos mais <strong>relevantes times de futebol do Brasil </strong>entre eles:</p>
+                    <div class="teams">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/logos-times.png" alt="Time brasileiros do campeonato Brasileirão série A">
+                    </div>
+                    <div class="box-s">
+                        <a href=""><span>Quero</span> Ser <strong>Afiliado</strong></a>
+                    </div>
+                </div>
+                <div class="col-img">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Jogadores.png" alt="Soares e Fernandinho">
+                </div>
+            </div>
+       </div>
+    </section>
+    <section class="benefits">
+        <h2>Benefícios</h2>
+        <div class="container">
+            <div class="owl-carousel owl-theme">
+                <div class="item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/avarage.jpg" alt="">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/24h.jpg" alt="">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/24h.jpg" alt="">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/24h.jpg" alt="">
+                </div>
+                <div class="item">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/more.jpg" alt="">
+                </div>
+            </div>
+               <div class="btn-affiliate">
+                    <div class="box-s">
+                        <a href=""><span>Quero</span> ser <strong></br>Afiliado</strong></a>
+                    </div>
+               </div>
+            </div>
+    </section>
+    <section class="games">
+        <h2>Mais de </br>3 Mil games</h2>
+        <div class="container">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/games.png" alt="Games">
+            <div class="btn-games">
+                <a href=""><span>I WANT TO</span> become <strong>AN AFFILIATE</strong></a>
+            </div>
+        </div>
+    </section>
+    <section class="competicoes">
+        <div class="container">
+            <div class="content-competicoes">
+                <div class="col-img">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/taca-papai.png" alt="">
+                </div>
+                <div class="col-info">
+                    <h3>Alcance além das <strong>quatro linhas</strong></h3>
+                    <p><strong>ÚNICA MARCA BRASILEIRA</strong> do segmento a anunciar na <strong>Libertadores da América</strong>, além de patrocínios e anúncios nos principais campeonatos estaduais, <strong>regionais e nacionais do país.</strong></p>
+                    <div class="btn-affiliate">
+                        <div class="box-s">
+                            <a href=""><span>Quero</span> ser <strong></br>Afiliado</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="eventos">
+        <div class="container content-event">
+            <div class="col-info">
+                <h3>Uma <strong>marca</strong> que <strong>fala </strong>com o <strong>povo </strong></h3>
+                <p>Queremos você <strong>festejando</strong> junto à nossa <strong>marca</strong>.</p>
+                <div class="btn-affiliate">
+                    <div class="box-s">
+                        <a href=""><span>Quero</span> ser <strong></br>Afiliado</strong></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-img">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/festa-esporte-02.png">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/festa-esporte-01.png">
+            </div>
+        </div>
+    </section>
+    <section class="social">
+        <div class="container">
+            <div class="content-social">
+                <div class="col-img">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/instagram.png" alt="Instagram Esporte da Sorte">
+                </div>
+                <div class="col-info">
+                    <h3>No <strong>mundo digital</strong>, a nossa <strong>marca é uma realidade</strong>.</h3>
+                    <p>O <strong>maior perfil brasileiro</strong> do segmento, <strong>no Instagram</strong>, é nosso. Mais de <strong>1 milhão seguidores prontos para quebrar novos recordes.</strong></p>
+                    <div class="btn-affiliate">
+                        <div class="box-s">
+                            <a href=""><span>Quero</span> ser <strong></br>Afiliado</strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="tv">
+        <div class="container">
+            <div class="content-tv">
+                <div class="col-info">
+                    <h3>No centro das <strong>atenções</strong></h3>
+                    <p>A <strong>primeira marca do segmento de apostas esportivas</strong> do país a adquirir cotas de patrocínio do <strong>Big Brother Brasil</strong>, um <strong>campeão de audiência e entretenimento.</strong></p>
+                    <div class="btn-affiliate">
+                        <div class="box-s">
+                            <a href=""><span>Quero</span> ser <strong></br>Afiliado</strong></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-img">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/TV.png" alt="TV / Big brother Brasil / BBB">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="afiliados">
+        <div class="content-afiliados">
+            <h2><span>Quero</span> ser <strong></br>Afiliado</strong></h2>
+            <a href="">I want to become an affiliate</a>
+        </div>
+    </section>
+</main> 
+
+<script>
         $('.owl-carousel').owlCarousel({
         loop:true,
-        autoplay:true,
-        margin:10,
+        autoplay:false,
+        margin:25,
         responsiveClass:true,
         dots:false,
         responsive:{
@@ -19,13 +169,13 @@
                 items:1,
             },
             700:{
-                items:2,
+                items:5,
             },
             1000:{
-                items:3,
+                items:5,
             }
          }
         })
-    </script> -->
+    </script>
 
 <?php get_footer()?>
